5835cabd279d9637ac37b44e2df36399daf870c2
Hryhorii <sagram.net@gmail.com>
2021-08-04 21:07:19 +0300

    Simplified the filter function

--- src/components/Books/BooksList.js
+++ src/components/Books/BooksList.js
@@ -9,9 +9,7 @@ import './BookItem.css'
 const BooksList = ({ onFilterButtonClick, selectedCategory = '' }) => {
     // console.log(selectedCategory)
 
-    const bookFiteredArray = bookArray.filter(({ category }) => {
-        return category === selectedCategory || selectedCategory === ''
-    })
+    // const bookFiteredArray =
 
     return (
         <>
@@ -25,25 +23,37 @@ const BooksList = ({ onFilterButtonClick, selectedCategory = '' }) => {
                               '. Click Home to return'}
                     </div>
                     <div className="row">
-                        {bookFiteredArray.map(
-                            ({ id, name, description, category, image }) => {
-                                {
-                                    return (
-                                        <BookItem
-                                            key={id}
-                                            // id={id}
-                                            name={name}
-                                            description={description}
-                                            category={category}
-                                            image={image}
-                                            onFilterButtonClick={
-                                                onFilterButtonClick
-                                            }
-                                        />
-                                    )
+                        {bookArray
+                            .filter(
+                                ({ category }) =>
+                                    category === selectedCategory ||
+                                    selectedCategory === ''
+                            )
+                            .map(
+                                ({
+                                    id,
+                                    name,
+                                    description,
+                                    category,
+                                    image,
+                                }) => {
+                                    {
+                                        return (
+                                            <BookItem
+                                                key={id}
+                                                // id={id}
+                                                name={name}
+                                                description={description}
+                                                category={category}
+                                                image={image}
+                                                onFilterButtonClick={
+                                                    onFilterButtonClick
+                                                }
+                                            />
+                                        )
+                                    }
                                 }
-                            }
-                        )}
+                            )}
                     </div>
                 </div>
             </div>
